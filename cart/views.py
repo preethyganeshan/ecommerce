180d0b493ea318587ce06704e8bcf45e181256ec
subhashiny <preethy.official7@gmail.com>
2022-12-01 10:47:33 +0530

    changes

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from ShoppingApp.models import Product
 from . models import Cart , CartItem
 from django.core.exceptions import  ObjectDoesNotExist
@@ -44,3 +44,23 @@ def cart_detail(request, total=0,counter=0,cart_items=None):
     except ObjectDoesNotExist:
         pass
     return render(request,'cart.html',dict(cart_items=cart_items,total=total,counter=counter))
+
+
+def cart_remove(request,product_id):
+    cart=Cart.objects.get(cart_id=_cart_id(request))
+    product=get_object_or_404(Product,id=product_id)
+    cart_item=CartItem.objects.get(product=product,cart=cart)
+    if cart_item.quantity >1:
+        cart_item.quantity -=1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart:cart_detail')
+
+
+def full_remove(request,product_id):
+    cart=Cart.objects.get(cart_id=_cart_id(request))
+    product=get_object_or_404(Product,id=product_id)
+    cart_item=CartItem.objects.get(product=product,cart=cart)
+    cart_item.delete()
+    return redirect('cart:cart_delete')
